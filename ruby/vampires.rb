9e870e1f735351843bcf62dca97c804223c97ee5
Prajay Soni <prajay.s@hotmail.com>
2016-05-08 15:01:10 -0400

    Add conditions to test for vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,14 +7,38 @@ age = gets.chomp
 puts "What year were you born in?"
 year = gets.chomp
 
-puts "Would you like some garlic bread from our cafeteria? (y/n)"
+puts "Would you like some garlic bread from our cafeteria? (yes/no)"
 garlic = gets.chomp
+if garlic.downcase == "yes"
+	garlic = true
+elsif garlic.downcase == "no"
+	garlic = false
+end
+		
 
-puts "Would you like to enroll in our health insuarance program?"
+puts "Would you like to enroll in our health insuarance program? (yes/no)"
 health = gets.chomp
+if health.downcase == "yes"
+	health = true
+elsif health.downcase == "no"
+	health = false
+end
+		
 
-p name
-p age
-p year
-p garlic
-p health
+correctAge = (2016 - year.to_i)
+age = age.to_i
+
+if age == correctAge && (garlic == true || health == true)
+	puts "Probably not a vampire"
+elsif (age != correctAge && garlic == false && health == false)
+	puts "Almost certainly a vampire"
+elsif age != correctAge && (garlic == false || health == false)
+	puts "Probably a vampire"
+elsif name.downcase == "drake cula" || name.downcase == "tu fang"
+	puts "Definitely a vampire"
+else
+	puts "Results inconclusive"
+end
+		
+		
+		
