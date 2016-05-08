05d28b3ca6a73f9b065279c6741dc711a645a04f
Prajay Soni <prajay.s@hotmail.com>
2016-05-08 14:17:34 -0400

    Add prompts for user input

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+puts "Please enter your name."
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born in?"
+year = gets.chomp
+
+puts "Would you like some garlic bread from our cafeteria? (y/n)"
+garlic = gets.chomp
+
+puts "Would you like to enroll in our health insuarance program?"
+health = gets.chomp
+
+p name
+p age
+p year
+p garlic
+p health
