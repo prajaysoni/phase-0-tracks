9e9621de32bd53607550bdd23bb5133ba95a3f7b
Prajay Soni <prajay.s@hotmail.com>
2016-05-08 15:29:45 -0400

    Add allergy loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -29,21 +29,37 @@ while employees > 0
 	elsif health.downcase == "no"
 		health = false
 	end
-			
+	
+	puts "Please enter any allergies you may have."		
+	allergy = gets.chomp
+	begin
+		if allergy.downcase == "done"
+			break
+		elsif allergy.downcase == "sunshine"
+			puts "Probably a vampire"
+			break
+		end
+		puts "Any others?"
+		allergy = gets.chomp
+	end until allergy.downcase == "done"	
+				
 	
 	correctAge = (2016 - year.to_i)
 	age = age.to_i
 	
-	if age == correctAge && (garlic == true || health == true)
-		puts "Probably not a vampire"
-	elsif (age != correctAge && garlic == false && health == false)
-		puts "Almost certainly a vampire"
-	elsif age != correctAge && (garlic == false || health == false)
-		puts "Probably a vampire"
-	elsif name.downcase == "drake cula" || name.downcase == "tu fang"
-		puts "Definitely a vampire"
-	else
-		puts "Results inconclusive"
+	if allergy.downcase == "done"
+
+		if age == correctAge && (garlic == true || health == true)
+			puts "Probably not a vampire"
+		elsif (age != correctAge && garlic == false && health == false)
+			puts "Almost certainly a vampire"
+		elsif age != correctAge && (garlic == false || health == false)
+			puts "Probably a vampire"
+		elsif name.downcase == "drake cula" || name.downcase == "tu fang"
+			puts "Definitely a vampire"
+		else
+			puts "Results inconclusive"
+		end
 	end
 
 	employees -= 1
