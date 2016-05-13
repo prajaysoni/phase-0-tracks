077f9da19102dcc7357132d0341284673d89f3ae
Prajay Soni <prajay.s@hotmail.com>
2016-05-13 15:07:11 -0400

    Add methods to first three strings

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,13 +4,13 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+ p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+ p "zom".insert(1, "o")
 # => “zoom”
 
-# "enhance".<???>
+ p "enhance".center(14)
 # => "    enhance    "
 
 # "Stop! You’re under arrest!".<???>
