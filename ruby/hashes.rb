528feb94102e1dfe51fa389f014c1a1d48e472f0
Prajay Soni <prajay.s@hotmail.com>
2016-05-14 11:29:11 -0400

    Create client hash and prompt user for input

---
+++ ruby/hashes.rb
@@ -0,0 +1,87 @@
+=begin
+
+Create hash named 'client' and add symbols
+	client = {name, age, number of kids, decor theme, apt or house?, number of rooms}
+
+Prompt user for data to fill the hash
+	Please enter your name:
+	Enter your age:
+	How many kids do you have?:
+	What theme do you want?:
+	Apartment or house?:
+	How many rooms?:
+
+Convert data to appropriate type
+	Age.to_i
+	Kids.to_i
+	Rooms.to_i
+
+Print hash back to user with data filled in.
+	p client[:name]
+	p client[:age]
+	...
+	...
+	...
+	p client[:num of rooms]
+
+Opportunity to update a key
+	If user says none
+		finish the program
+	Elsif whatever key in inputted
+		give user the option to update the key
+	End
+	
+=end
+
+
+client = {}
+			
+puts "Please enter your name"
+client[:name] = gets.chomp
+
+puts "Enter your age"
+client[:age] = gets.chomp
+
+puts "How many children do you have?"
+client[:kids] = gets.chomp
+
+puts "What type of theme do you want?"
+client[:decorTheme] = gets.chomp
+
+puts "Do you live in an apartment or a house?"
+client[:residence] = gets.chomp
+
+puts "How many rooms in your home?"
+client[:numRooms] = gets.chomp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
