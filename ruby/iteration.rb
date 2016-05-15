e29889b4edfa40e5f2f600da45bc4f1d9ca6343f
Prajay Soni <prajay.s@hotmail.com>
2016-05-14 20:15:23 -0400

    Add statements for release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,23 @@ def iteration
 end
 
 iteration {puts "Now we are outside of the method and we finished out block!!!"}
+
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+names = {
+	"Dad" => "Bob",
+	"Mom" => "Cindy",
+	"Sister" => "Katy"
+}
+
+p days
+days.each { |day| puts "On #{day}, we will code!!" }
+
+p names
+names.each { |relation, name| puts "My #{relation}'s name is #{name}." }
+
+puts "Lets reverse our days of the week."
+
+new_day = days.map! { |day| day.reverse }
+p new_day
