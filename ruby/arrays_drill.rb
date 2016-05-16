8d5b3c805373a5bbd4bbb7795ded2469e38124d6
Prajay Soni <prajay.s@hotmail.com>
2016-05-16 17:16:52 -0400

    Add more release one items

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,19 @@
 test = []
-
 p test
+
+test << 1 << 2 << 3 << 4 << 5
+p test
+
+test.delete_at(2)
+p test
+
+test.insert(2, 100)
+p test
+
+test.delete(1)
+p test
+
+if test.include?(100)
+	puts "100 is in the array"
+end
+ 
