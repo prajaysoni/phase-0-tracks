ea3642aaea7fd27e716d16fe93b94573ef3b31cc
Prajay Soni <prajay.s@hotmail.com>
2016-05-16 15:52:56 -0400

    Add print commands to test nesting and add an array of hashes for Ricky Bobby

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -26,7 +26,38 @@ classroom = {
 			'Hot Dogs',
 			'French Fries',
 			'Beer'
+		],
+		'Races' => [
+			'Indiana Jones 500' => {
+				'Fastest' => '2:45',
+				'Slowest' => '3:05'
+			},
+			'Bob Saget 1000' => {
+				'Fastest' => '2:35',
+				'Slowest' => '2:48'
+			},
+			'Bob Belcher 100' => {
+				'Fastest' => '4:15',
+				'Slowest' => '5:03'
+			}
 		]
 	}
 
 }
+
+puts "Info for Billy Bob"
+p classroom['Billy Bob']['Favorite subject']
+
+p classroom['Billy Bob']['Grade']
+
+p classroom['Billy Bob']['Foods'][1]
+
+
+puts "Info for Won Reasley"
+p classroom['Won Reasley']['Favorite subject']
+
+p classroom['Won Reasley']['Grade']
+
+p classroom['Won Reasley']['Foods'][2]
+
+p classroom['Won Reasley']['Foods']
