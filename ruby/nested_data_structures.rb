f0beb20ba57d413226a01d11f84b1cb8d5f5b020
Prajay Soni <prajay.s@hotmail.com>
2016-05-16 15:41:17 -0400

    Add a classroom hash with three nested student hashes.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+classroom = {
+	'Billy Bob' => {
+		'Favorite subject' => 'Math',
+		'Grade' => 'A',
+		'Foods' => [
+			'Pizza',
+			'Burgers',
+			'McDonalds'
+		]
+	},
+
+	'Won Reasley' => {
+		'Favorite subject' => 'History',
+		'Grade' => 'C',
+		'Foods' => [
+			'Pho',
+			'Curry',
+			'Apple Pie'
+		]
+	},
+
+	'Ricky Bobby' => {
+		'Favorite subject' => 'Cars',
+		'Grade' => 'AAA',
+		'Foods' => [
+			'Hot Dogs',
+			'French Fries',
+			'Beer'
+		]
+	}
+
+}
