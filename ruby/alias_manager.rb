da7f263522f382efad437732ba64f2e9d738156d
Prajay Soni <prajay.s@hotmail.com>
2016-05-17 10:06:08 -0400

    Wrap previous code inside until loop to provide user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -12,24 +12,35 @@ Use gsub method on name to replace consonants with next one. Skip vowels
 Use split on name to make an array of length two. Then use map and capitalize to capitalize the first letter.
 Then use join to make name into a string again.
 
+Wrap above code inside an until loop
+Until the name entered equals 'quit', keep taking names
+
 
 	
 =end
 
+puts "Welcome to the Spy Name Generator 1000"
+puts "Please enter your name, or if you wish to leave, enter 'quit'"
+realName = gets.chomp
 
-def spyName(name)
-	name = name.downcase
-	spy = name.split(' ')
-	name = spy.rotate
-	name = name.join(' ')
-	name = name.gsub(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
-	name = name.gsub(/[bcdfghjklmnpqrstvwxyz]/,'b' => 'b'.next, 'c' => 'c'.next, 'd' => 'e'.next, 'f' => 'f'.next,
-	 'g' => 'g'.next, 'h' => 'i'.next, 'j' => 'j'.next, 'k' => 'k'.next, 'l' => 'l'.next, 'm' => 'm'.next, 'n' => 'o'.next,
-	 'p' => 'p'.next, 'q' => 'q'.next, 'r' => 'r'.next, 's' => 's'.next, 't' => 'u'.next, 'v' => 'v'.next, 'w' => 'w'.next,
-	 'x' => 'x'.next, 'y' => 'y'.next, 'z' => 'z'.next )
-	name = name.split(' ').map{ |word| word.capitalize }.join(' ')
+until realName.downcase == "quit"
 
-
+	def spyName(name)
+		name = name.downcase
+		spy = name.split(' ')
+		name = spy.rotate
+		name = name.join(' ')
+		name = name.gsub(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
+		name = name.gsub(/[bcdfghjklmnpqrstvwxyz]/,'b' => 'b'.next, 'c' => 'c'.next, 'd' => 'e'.next, 'f' => 'f'.next,
+		 'g' => 'g'.next, 'h' => 'i'.next, 'j' => 'j'.next, 'k' => 'k'.next, 'l' => 'l'.next, 'm' => 'm'.next, 'n' => 'o'.next,
+		 'p' => 'p'.next, 'q' => 'q'.next, 'r' => 'r'.next, 's' => 's'.next, 't' => 'u'.next, 'v' => 'v'.next, 'w' => 'w'.next,
+		 'x' => 'x'.next, 'y' => 'y'.next, 'z' => 'z'.next )
+		name = name.split(' ').map{ |word| word.capitalize }.join(' ')
+	
+	
+	end
+	
+	puts "Your spy name is: #{spyName(realName)}."
+	puts "Enter another name or enter 'quit'"
+	realName = gets.chomp
 end
-
-p spyName("Felicia Torres")
