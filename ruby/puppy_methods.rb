22ae73dbb765374dd271f94fddcf70eb3c2958fd
Prajay Soni <prajay.s@hotmail.com>
2016-05-18 13:23:53 -0400

    Add rest of do the thing solutions

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -11,9 +11,30 @@ class Puppy
   	end
   end
 
+  def roll_over()
+  	puts "*rolls over*"
+  end
+
+  def dog_years(int)
+  	int = int.to_f
+  	dog_age = int/7
+  	dog_age = dog_age.round(2)
+  	puts "The dog is #{dog_age} years old"
+  	dog_age
+  end
+
+  def dog_eats(mins)
+  	puts "It took the dog #{mins} minutes to eat the food."
+  end
 end
 
 pup = Puppy.new
 pup.fetch("bone")
 
 pup.speak(5)
+
+pup.roll_over()
+
+pup.dog_years(50)
+
+pup.dog_eats(10)
