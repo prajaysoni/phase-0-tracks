46401f684c43d525d248b2c2e16a6638bc98dc00
Prajay Soni <prajay.s@hotmail.com>
2016-05-18 13:57:21 -0400

    Add release two solutions

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -26,8 +26,44 @@ class Puppy
   def dog_eats(mins)
   	puts "It took the dog #{mins} minutes to eat the food."
   end
+
+  def initialize()
+  	puts "initializing new puppy instance..."
+  end
+
 end
 
+class Kittens
+
+	def initialize()
+		puts "Initializing new kitten instance..."
+	end
+
+	def kitten_fetch(item)
+		puts "The kitten brought back a #{item}"
+	end
+
+	def kitten_lives(lives)
+		if lives > 9
+			puts "Cats only have 9 lives. You're wrong!"
+		elsif lives <= 0
+			puts "Your cat's already dead!"
+		else
+			puts "Your cat has #{lives} lives left"
+		end
+	end	
+
+
+
+
+
+
+end
+
+
+
+
+=begin 
 pup = Puppy.new
 pup.fetch("bone")
 
@@ -38,3 +74,23 @@ pup.roll_over()
 pup.dog_years(50)
 
 pup.dog_eats(10)
+=end
+
+cat = Kittens.new
+
+cat.kitten_fetch("mouse")
+cat.kitten_lives(6)
+cat.kitten_lives(0)
+cat.kitten_lives(20)
+
+idx = 0
+kitten_array = []
+until idx == 50
+
+	cat = Kittens.new
+	idx += 1
+	kitten_array << cat
+	
+end
+kitten_array.each {|cat| cat.kitten_fetch("mouse")}
+kitten_array.each {|cat| cat.kitten_lives(7)}
