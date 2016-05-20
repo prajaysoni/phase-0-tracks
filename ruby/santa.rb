6fd4518e90e5a0c1319322e6917a6d7fadaa7691
Prajay Soni <prajay.s@hotmail.com>
2016-05-20 14:34:37 -0400

    Add initialization to see if Santa is a robot

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,20 @@
 class Santa
 
+	def initialize(gender, ethnicity, robot)
+		puts "initializing Santa instance"
+		@robot = robot
+		if @robot.downcase == "yes"
+			puts "This Santa is a robot"
+		else
+			puts "This Santa is a human"
+		end
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
+							 "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak()
 		puts "Ho, ho, ho!. Haaaappy holidays!"
 	end
@@ -8,14 +23,16 @@ class Santa
 		puts "That was a good #{type}"
 	end
 
-	def initialize()
-		puts "initializing Santa instance"
-	end
-
 end
 
-nick = Santa.new
 
-nick.speak()
-nick.eat_milk_and_cookies("Chocolate Chip")
-nick.initialize()
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities =["black", "Latino", "white", "Japanese-African", 
+					  "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+	puts "Is this Santa a robot?"
+	answer = gets.chomp
+	santas << Santa.new(example_genders[i], example_ethnicities[i], answer)
+	puts "---------"
+end
