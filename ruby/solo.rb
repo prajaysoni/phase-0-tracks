ae9564ee00cf622088fb70566df81258eedcdb02
Prajay Soni <prajay.s@hotmail.com>
2016-05-20 19:36:52 -0400

    Initial commit. Add class pseudocode for release 0

---
+++ ruby/solo.rb
@@ -0,0 +1,19 @@
+=begin
+
+Class name: Superhero
+
+Initialize method
+	Take (name, age, power)
+
+Powerup method
+	Take (additional power)
+
+Weakness method
+	Take (random weakness from array)
+
+Villain method
+	Take (random villain from array)
+
+End
+
+=end
