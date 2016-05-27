89a58e455b531b9bdc6d302d9b22b78a21bb6d81
Prajay Soni <prajay.s@hotmail.com>
2016-05-27 19:28:59 -0400

    Add function code for reversing string

--- js/explore.js
+++ js/explore.js
@@ -13,3 +13,13 @@ When the loop is done, print the new string out
 
 */
 
+function reverseString(str) {
+	var newStr = "";
+	var origStr = str;
+	for (var i = origStr.length - 1; i >= 0; i--) {
+		newStr = newStr + origStr[i];
+	}
+	return newStr;
+}
+
+console.log(reverseString("hello world"));
