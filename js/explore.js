b1593fe8705b7d99c13189becc79a58b2662cea5
Prajay Soni <prajay.s@hotmail.com>
2016-05-27 17:58:27 -0400

    Add pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+/*
+
+Create reverse function
+Input: string
+Output: string reversed
+
+The function takes in a string as an argument
+Create a variable to store the reversed string
+Create a loop
+For each letter of the string, starting at the end
+Add that letter to the new variable
+When the loop is done, print the new string out
+
+*/
+
