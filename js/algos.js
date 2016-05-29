53130b57f3cd144ebc645bd055627c29097abf69
Prajay Soni <prajay.s@hotmail.com>
2016-05-28 23:55:11 -0400

    Initial commit. Add pseudocode for longest phrase function

---
+++ js/algos.js
@@ -0,0 +1,18 @@
+/*
+
+Longest phrase function
+Input: An array of strings
+Output: Longest string from array
+
+	Start the function with one argument
+	Create an array with the value of the argument
+	Create a variable to hold the longest phrase
+	Create a loop to go through each element in the array
+	Assign the first value of the array to the longest phrase variable
+	Test to see if this is bigger than the next
+		If it is, then move to the next
+		Else, assign the next value to the variable, then move on
+	When the loop is done, the variable should hold the longest string
+
+*/
+
