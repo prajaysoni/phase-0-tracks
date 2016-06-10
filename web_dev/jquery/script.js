81a1327fb5185f68079a33d809422e03e316aec9
Prajay Soni <prajay.s@hotmail.com>
2016-06-10 12:41:56 -0400

    Add jquery to page to test different methods

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,1 +1,32 @@
 console.log("Hello");
+
+$(function() {
+
+	$('#btn1').on('click', function() {
+		$('#data1').slideToggle(500);
+	});
+
+	$('#btn2').on('click', function() {
+		$('#data2').toggle(1000);
+	});
+
+	$('#btn3').on('click', function() {
+		$('#data3').toggleClass('red');
+	});
+
+	$('#head').hover( function() {
+		$(this).toggleClass('red');
+	});
+
+
+	function fade() {
+		$('#fade').fadeOut(1500, function() {
+
+			$('#fade').delay(500).fadeIn(1500);
+			fade();
+
+		});
+	}
+	fade();
+
+});
