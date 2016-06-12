ef852323bcdc2e4addfddee56d3a6e01409ddb80
Prajay Soni <prajay.s@hotmail.com>
2016-06-12 13:19:42 -0400

    Add contact and great_job GET routes

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,20 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact' do
+  "123 Random Lane <br> Anytown, NJ 01234"
+end
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+
+
+
